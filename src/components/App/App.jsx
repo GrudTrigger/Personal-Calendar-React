3b8cf0896d51e7f964242cf38e85d873d59a0c4d
George <92686556+GrudTrigger@users.noreply.github.com>
2023-07-24 21:31:19 +0300

    change Form and add Event

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,11 +5,18 @@ import { useEffect, useState } from "react";
 import moment from "moment";
 
 const url = "http://localhost:5000";
-
+const defaultEvent = {
+  title: "",
+  descr: "",
+  date: Number(moment().format("X")),
+};
 const App = () => {
   moment.updateLocale("en", { week: { dow: 1 } });
   const [events, setEvents] = useState([]);
   const [selectedMonth, setSelectedMonth] = useState(moment());
+  const [event, setEvent] = useState(null);
+  const [method, setMethod] = useState(null);
+  const [isOpenModal, setIsOpenModal] = useState(false);
   const startDay = selectedMonth.clone().startOf("month").startOf("week");
   const startDayQuery = startDay.clone().format("X");
   const endDayQuery = startDay.clone().add(42, "days").format("X");
@@ -32,25 +39,118 @@ const App = () => {
     setSelectedMonth((prevMonth) => prevMonth.clone().add(1, "month"));
   };
 
+  const openFormHandler = (methodName, eventForUpdate) => {
+    console.log("click", methodName);
+    setIsOpenModal(true);
+    setEvent(eventForUpdate || defaultEvent);
+    setMethod(methodName);
+  };
+
+  const cancelButtonHandler = () => {
+    setIsOpenModal(false);
+    setEvent(null);
+  };
+
+  const changeEventHandler = (text, field) => {
+    setEvent((prev) => ({
+      ...prev,
+      [field]: text,
+    }));
+  };
+
+  const eventFetchHandler = () => {
+    const fetchUrl =
+      method === "Update" ? `${url}/events/${event.id}` : `${url}/events`;
+    const httpMethod = method === "Update" ? "PATCH" : "POST";
+
+    fetch(fetchUrl, {
+      method: httpMethod,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(event),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (method === "Update") {
+          setEvents((prev) =>
+            prev.map((eventEl) => (eventEl.id === res.id ? res : eventEl)),
+          );
+        } else {
+          setEvents((prev) => [...prev, res]);
+        }
+        cancelButtonHandler();
+      });
+  };
+
+  console.log(events);
+  console.log(moment().format("X"));
   return (
-    <div className="w-[1200px] h-[932px] rounded-2xl bg-white border my-0 mx-auto pl-7 pr-7 mt-6">
-      <header>
-        <Header
-          prevHandler={prevHandler}
-          todayHandler={todayHandler}
-          nextHandler={nextHandler}
-          today={selectedMonth}
-        />
-      </header>
-      <main>
-        <DayOfTheWeek />
-        <DaysOfMonth
-          startDay={startDay}
-          selectedMonth={selectedMonth}
-          events={events}
-        />
-      </main>
-    </div>
+    <>
+      {isOpenModal ? (
+        <div
+          onClick={cancelButtonHandler}
+          className="absolute z-50 top-0 right-0 bottom-0 left-0 flex items-center justify-center bg-black bg-opacity-30"
+        >
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className="w-72 bg-white text-black rounded-lg shadow-md p-4"
+          >
+            <input
+              onChange={(e) => {
+                changeEventHandler(e.target.value, "title");
+              }}
+              value={event.title}
+              type="text"
+              className="w-full px-3 py-2 text-sm border-b border-gray-300 focus:border-black focus:outline-none"
+              placeholder="Имя"
+            />
+            <input
+              onChange={(e) => {
+                changeEventHandler(e.target.value, "descr");
+              }}
+              value={event.descr}
+              type="text"
+              className="w-full mt-3 px-3 py-2 text-sm border-b border-gray-300 focus:border-black focus:outline-none"
+              placeholder="Email"
+            />
+            <div className="py-2 px-4 flex justify-between">
+              <button
+                onClick={cancelButtonHandler}
+                className="mt-4 w-35 px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={eventFetchHandler}
+                className="mt-4 w-35 px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
+              >
+                {method}
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : null}
+      <div className="w-[1200px] h-[932px] rounded-2xl bg-white border my-0 mx-auto pl-7 pr-7 mt-6">
+        <header>
+          <Header
+            prevHandler={prevHandler}
+            todayHandler={todayHandler}
+            nextHandler={nextHandler}
+            today={selectedMonth}
+          />
+        </header>
+        <main>
+          <DayOfTheWeek />
+          <DaysOfMonth
+            startDay={startDay}
+            selectedMonth={selectedMonth}
+            events={events}
+            openFormHandler={openFormHandler}
+          />
+        </main>
+      </div>
+    </>
   );
 };
 
--- src/components/DaysOfMonth/DaysOfMonth.jsx
+++ src/components/DaysOfMonth/DaysOfMonth.jsx
@@ -1,13 +1,7 @@
 import clsx from "clsx";
 import moment from "moment";
-import AddIcon from "../icons/AddIcon";
-import { useState } from "react";
-import { Dialog } from "@headlessui/react";
 
-const DaysOfMonth = ({ startDay, selectedMonth, events }) => {
-  const [hoveredIndex, setHoveredIndex] = useState(null);
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
+const DaysOfMonth = ({ startDay, selectedMonth, events, openFormHandler }) => {
   const totalDay = 42;
   const day = startDay.clone().subtract(1, "day");
   const daysArray = [...Array(totalDay)].map((_, index) =>
@@ -34,53 +28,36 @@ const DaysOfMonth = ({ startDay, selectedMonth, events }) => {
           <DaysCell
             key={index}
             isWeekend={dayItem.day() === 6 || dayItem.day() === 0}
-            onMouseEnter={() => setHoveredIndex(index)}
-            onMouseLeave={() => setHoveredIndex(null)}
           >
-            {hoveredIndex === index && numberDay === dayItem.month() && (
-              <AddIcon
-                setIsOpenModal={setIsOpenModal}
-                onClick={() => setIsOpenModal(true)}
-                className="mr-20 cursor-pointer"
-              />
-            )}
             <Day
               today={isToday}
               dayItem={dayItem}
               isCurrentDay={isCurrentDay}
               numberDay={numberDay}
+              openFormHandler={openFormHandler}
             ></Day>
+            <ul className="m-0 list-inside p-1">
+              {events
+                .filter(
+                  (event) =>
+                    event.date >= dayItem.format("X") &&
+                    event.date <= dayItem.clone().endOf("day").format("X"),
+                )
+                .map((event, index) => (
+                  <button
+                    key={index}
+                    onClick={() => openFormHandler("Update", event)}
+                    className=" relative -left-1 text-ellipsis overflow-hidden whitespace-nowrap w-28 border-0 text-green-400 cursor-pointer m-0 p-0 text-left"
+                  >
+                    <li key={event.id} className="">
+                      {event.title}
+                    </li>
+                  </button>
+                ))}
+            </ul>
           </DaysCell>
         );
       })}
-      <Dialog open={isOpenModal} onClose={() => setIsOpenModal(false)}>
-        <div className=" fixed left-0 right-0 top-0 bottom-0 flex items-center justify-center bg-black bg-opacity-50">
-          <Dialog.Panel className="bg-white border rounded-2xl p-7">
-            <Dialog.Title className="font-text text-xl text-center mb-4">
-              Add event
-            </Dialog.Title>
-            <input
-              className=" w-[350px] border-b border-black focus:border-black focus:outline-none"
-              type="text"
-              placeholder="Title"
-            />
-            <div className="flex justify-between items-center mt-4 gap-3">
-              <button
-                className=" w-40 h-12 border-2 rounded-2xl border-green-400 text-black font-title font-normal hover:bg-green-300"
-                onClick={() => setIsOpenModal(false)}
-              >
-                Save
-              </button>
-              <button
-                className=" w-40 h-12 border-2 rounded-2xl border-grey-200 hover:bg-gray-300"
-                onClick={() => setIsOpenModal(false)}
-              >
-                Cancel
-              </button>
-            </div>
-          </Dialog.Panel>
-        </div>
-      </Dialog>
     </DaysOfMonthGrid>
   );
 };
@@ -91,32 +68,25 @@ const DaysOfMonthGrid = ({ children }) => {
   return <div className="grid grid-cols-7 grid-rows-6 gap-1">{children}</div>;
 };
 
-const DaysCell = ({ children, isWeekend, onMouseEnter, onMouseLeave }) => {
+const DaysCell = ({ children, isWeekend }) => {
   const classCell = clsx(
-    "w-31 h-32 border rounded-xl border-gray-200 pr-3 pl-1 pt-2",
+    "w-31 h-32 border rounded-xl border-gray-200 pr-3 pl-1 pt-2 flex flex-col",
     isWeekend ? "bg-gray-300" : "bg-white",
   );
-  return (
-    <div className={classCell}>
-      <div
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-        className="h-7 flex justify-end"
-      >
-        {children}
-      </div>
-    </div>
-  );
+  return <div className={classCell}>{children}</div>;
 };
 
-const Day = ({ dayItem, isCurrentDay, numberDay, children }) => {
+const Day = ({ dayItem, isCurrentDay, numberDay, openFormHandler }) => {
   const isToday = isCurrentDay(dayItem);
   const classDay = clsx(
-    "w-7 h-7 font-title font-bold  flex justify-center items-center",
+    "w-7 h-7 font-title font-bold",
     numberDay === dayItem.month() ? "opacity-100" : "opacity-30",
   );
   return (
-    <div>
+    <div
+      onClick={() => openFormHandler("Create")}
+      className="flex justify-end cursor-pointer"
+    >
       {isToday ? (
         <div className="w-7 h-7 font-title font-bold flex items-center justify-center bg-red-500 border rounded-full">
           {dayItem.format("D")}
@@ -127,32 +97,3 @@ const Day = ({ dayItem, isCurrentDay, numberDay, children }) => {
     </div>
   );
 };
-
-const Event = () => {
-  return (
-    <div>
-      <p>qwertyuio</p>
-    </div>
-  );
-};
-
-// const Day = ({ dayItem, isCurrentDay, numberDay, children }) => {
-//   const isToday = isCurrentDay(dayItem);
-//   const classDay = clsx(
-//     "w-7 h-7 font-title font-bold  flex justify-center items-center",
-//     numberDay === dayItem.month() ? "opacity-100" : "opacity-30",
-//   );
-//   return (
-//     <div className="h-7">
-//       <div className="mx-auto">
-//         {isToday ? (
-//           <div className="w-7 h-7 font-title font-bold flex items-center justify-center bg-red-500 border rounded-full">
-//             {dayItem.format("D")}
-//           </div>
-//         ) : (
-//           <div className={classDay}>{dayItem.format("D")}</div>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
