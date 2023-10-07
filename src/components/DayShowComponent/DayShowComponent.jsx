d1e641c5b040ea8caad2f323972fb7807f6b059a
George <92686556+GrudTrigger@users.noreply.github.com>
2023-10-07 15:37:53 +0300

    add redline

--- src/components/DayShowComponent/DayShowComponent.jsx
+++ src/components/DayShowComponent/DayShowComponent.jsx
@@ -1,5 +1,6 @@
 import FormEvent from "../FormEvent/FormEvent";
 import moment from "moment";
+import {useEffect, useState} from "react";
 
 const DayShowComponent = ({
   events,
@@ -29,9 +30,29 @@ const DayShowComponent = ({
     return temp;
   });
 
+  const getRedLinePosition = () => ((moment().format('X') - today.format('X')) / 86400) * 100
+  const isDayContaintCurrentTimestamp = (x, b) => {
+    return  x >= b.startOf('day').format('X') && x <= b.clone().endOf('day').format('X');
+  }
+
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    const timerId = setInterval(()=> {
+      setCounter(prevState => prevState + 1)
+    }, 1000)
+
+    return () => clearInterval(timerId)
+  }, []);
+
   return (
     <div className="flex justify-between pt-4">
-      <div className=" w-[665px] h-full flex flex-col pl-1 pr-1">
+      <div className=" w-[665px] h-full flex flex-col pl-1 pr-1 relative">
+        {
+          isDayContaintCurrentTimestamp(moment().format('X'), today) ? (
+              <div className="bg-red-600 h-[1px] absolute left-0 right-0" style={{top: `${getRedLinePosition()}%`}}></div>
+          ) : null
+        }
         {cells.map((eventsList, i) => (
           <div key={i} className=" flex-grow relative border-t ml-8">
             <div className=" absolute -left-7 -top-[10px] text-xs">
